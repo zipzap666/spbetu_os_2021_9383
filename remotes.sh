4898f674df175db38bf7089f45e355e2d6ac673f
Mikhail Efremov <jakutenshi@gmail.com>
2021-02-11 01:50:12 +0300

    Init

---
+++ remotes.sh
@@ -0,0 +1,25 @@
+git remote add FollowJust                     https://github.com/FollowJust/spbetu_comparch_2020_9383.git               # Гладких
+git remote add Hotiakov                       https://github.com/Hotiakov/spbetu_comparch_2020_9383.git                 # Хотяков
+git remote add KamzolovNikita                 https://github.com/KamzolovNikita/spbetu_comparch_2020_9383.git           # Камзолов
+git remote add kirill-1103                    https://github.com/kirill-1103/spbetu_comparch_2020_9383.git              # Крейсманн
+git remote add Kirill-Mois                    https://github.com/Kirill-Mois/spbetu_comparch_2020_9383.git              # Моисейченко
+git remote add Anny45                         https://github.com/Anny45/spbetu_comparch_2020_9383.git                   # Карпекина
+git remote add Ciratoku                       https://github.com/Ciratoku/spbetu_comparch_2020_9383.git                 # Гордон
+git remote add emptyfs                        https://github.com/emptyfs/spbetu_comparch_2020_9383.git                  # Корсунов
+git remote add chebesovairina                 https://github.com/chebesovairina/spbetu_comparch_2020_9383.git           # Чебесова
+git remote add BasilNozdrin                   https://github.com/BasilNozdrin/spbetu_comparch_2020_9383.git             # Ноздрин
+git remote add AlexandraLysova                https://github.com/AlexandraLysova/spbetu_comparch_2020_9383.git          # Лысова
+git remote add sttie                          https://github.com/sttie/spbetu_comparch_2020_9383.git                    # Арутюнян
+git remote add didges                         https://github.com/didges/spbetu_comparch_2020_9383.git                   # Поплавский
+git remote add Anastasialapina                https://github.com/Anastasialapina/spbetu_comparch_2020_9383.git          # Лапина
+git remote add AnastasiaSergiencova2305       https://github.com/AnastasiaSergiencova2305/spbetu_comparch_2020_9383.git # Сергиенкова
+git remote add Machumak00                     https://github.com/Machumak00/spbetu_comparch_2020_9383.git               # Чумак
+git remote add RomanRybnikov                  https://github.com/RomanRybnikov/spbetu_comparch_2020_9383.git            # Рыбников
+git remote add heesus                         https://github.com/heesus/spbetu_comparch_2020_9383.git                   # Орлов
+git remote add vasilysamulevich               https://github.com/vasilysamulevich/spbetu_comparch_2020_9383.git         # Самулевич
+git remote add sfpnmrnk                       https://github.com/sfpnmrnk/spbetu_comparch_2020_9383.git                 # Пономаренко
+#git remote add                                https://github.com//spbetu_comparch_2020_9383.git
+#git remote add                                https://github.com//spbetu_comparch_2020_9383.git
+#git remote add                                https://github.com//spbetu_comparch_2020_9383.git
+#git remote add                                https://github.com//spbetu_comparch_2020_9383.git
+#git remote add                                https://github.com//spbetu_comparch_2020_9383.git
